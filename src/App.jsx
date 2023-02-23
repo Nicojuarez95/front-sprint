a1c758075152123654315d1ccee76c3e9fdb6e64
Nico <juareznico61@gmail.com>
2023-02-23 18:56:35 -0300

    arrancando hero

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='seccionHero-1'>
+        <nav>
+            
+        </nav>
+        <section>
+
+        </section>
+    </div>
+  )
+}
