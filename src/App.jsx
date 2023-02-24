3d4f0721e3925178c4a8678034213cde42223403
Nico <juareznico61@gmail.com>
2023-02-24 17:40:15 -0300

    Index listo todo responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import './App.css'
-import Hero1 from './Components/Hero1/Hero1'
-import Hero2Register from './Components/Hero2Register/Hero2Register';
+import Index from './Components/Index/Index';
 
 
 export default function App() {
   
   return (
-    <>
-    <Hero1/>
-    <Hero2Register/>
-    </>
+    <Index/>
   )
 }
