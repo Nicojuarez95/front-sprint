cd382590ac7376dd0af31393496db99d238d9832
Nicolás Juárez <112711432+Nicojuarez95@users.noreply.github.com>
2023-02-24 17:47:22 -0300

    M 03 (#3)
    * register listo
    
    * Index listo todo responsive

---
+++ src/Components/Index/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero1 from '../Hero1/Hero1'
+import Hero2Register from '../Hero2Register/Hero2Register'
+
+export default function Index() {
+  return (
+    <div>
+        <Hero1/>
+        <Hero2Register/>
+    </div>
+  )
+}
