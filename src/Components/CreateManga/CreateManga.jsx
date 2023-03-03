ad05b7308b9a6ebfc2b6218cea82d89e80b13f8a
Feermeringer <Tu correo>
2023-03-03 20:37:22 -0300

    cambios

--- src/Components/CreateManga/CreateManga.jsx
+++ src/Components/CreateManga/CreateManga.jsx
@@ -4,8 +4,7 @@ import './CreateManga.css';
 import axios from 'axios';
 
 export default function CreateManga() {
-    const [categorias, setCategorias ] = useState([])
-    const [selectValue, setSelectValue] = useState([]);
+    const [categories, setCategories] = useState([]);
     let title = useRef();
     let category = useRef();
     let description = useRef();
@@ -18,62 +17,61 @@ export default function CreateManga() {
             category: category.current.value,
             description: description.current.value,
             coverPhoto: coverPhoto.current.value,
-            
         };
+        
         console.log(manga);
-        const url = 'http://localhost:8000/manga'
-    try {
-        await axios.post(url, manga)
-        alert("Manga created successfully")
-            // e.target.reset()
-            console.log(setSelectValue())
+        const url = 'http://localhost:8000/mangas';
     
-    } catch (error) {
-        // console.log(error)
-        console.log("ocurrio un error")
+        try {
+            await axios.post(url, manga, {
+            });
+            alert('Manga created successfully');
+            console.log(category.current.value);
+        } catch (error) {
+            console.log('ocurrio un error');
+        }
     }
-    }
+
     const handleSelectChange = (event) => {
-        setSelectValue(console.log(event.target.value));
-        
+        // No es necesario guardar el valor del select en una variable de estado
+        console.log(event.target.value);
     };
-    
-    
-    useEffect( ()=>  { 
-        axios.get ('http://localhost:8000/categorias').then((response) => 
-        { setCategorias(response.data) } )
-        
 
-        
-    }, [] ) 
-    // console.log(categorias)
+    useEffect(() => {
+        axios.get('http://localhost:8000/categories').then((response) => {
+            setCategories(response.data);
+        });
+    }, []);
 
-return (
-    <div className='content-form'>
-        <h1>New Manga</h1>
-        <form onSubmit={handleSubmit}>
-            <fieldset>
-                <input className='inputMove' type='text' placeholder='Insert title' ref={title} />
-            </fieldset>
-            <fieldset>
-                <select className='inputMove' id='selectMove' ref={category} value={selectValue} onChange={handleSelectChange}>
-                <option value=''> Insert category</option>
-                    {categorias.map(categoria =>  <option key={categoria.value} value={categoria.value}>{categoria.name}</option>
-                    )}
-                    {/* <option value='action'>Action</option>
-                    <option value='adventure'>Adventure</option>
-                    <option value='comedy'>Comedy</option>
-                    <option value='drama'>Drama</option> */}
-                </select>
-            </fieldset>
-            <fieldset>
-                <input className='inputMove' type='text' placeholder='Insert description' ref={description} />
-            </fieldset>
-            <fieldset>
-                <input className='inputMove' type='text' placeholder='Insert cover photo' ref={coverPhoto} />
-            </fieldset>
-            <button className='btn-manga' type='submit'>Send</button>
-        </form>
-    </div>
-);
+
+    return (
+        <div className='content-form'>
+            <h1>New Manga</h1>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <input className='inputMove' type='text' placeholder='Insert title' ref={title} />
+                </fieldset>
+                <fieldset>
+                    <select className='inputMove' id='selectMove' ref={category} onChange={handleSelectChange}>
+                        <option value=''>Select a category</option>  
+                        { categories.length > 0 && categories.map(category => (
+                            <option key={category._id} value={category.value}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </select>
+                </fieldset>
+
+                <fieldset>
+                    <input className='inputMove' type='text' placeholder='Insert description' ref={description} />
+                </fieldset>
+                <fieldset>
+                    <input className='inputMove' type='text' placeholder='Insert cover photo' ref={coverPhoto} />
+                </fieldset>
+                <button className='btn-manga' type='submit'>
+                    Send
+                </button>
+            </form>
+        </div>
+    );
 }
