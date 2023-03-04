b2217f8eae3c7e580cb2168133db6252429604ef
Nicolás Juárez <112711432+Nicojuarez95@users.noreply.github.com>
2023-03-04 18:28:51 -0300

    de user-dev a user (#5)
    * Auth listo
    
    * Auth listo y responsive
    
    * password modificado
    
    * form login casi listo
    
    * solucionado errores de rutas de componentes y avance en fromlogin
    
    * todo listo falta hacer dinamico el perfil de navbar
    
    * front listo

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,12 +3,15 @@ import Hero2Register from "./Hero2Register/Hero2Register";
 import Index from "./Index/Index";
 import NotFound from "./NotFound/NotFound"
 import IndexLayout from "../Layouts/IndexLayouts/IndexLayouts"
+import Auth from "./Auth/Auth";
+import MainLayout from '../Layouts/MainLayouts/MainLayouts'
+
 
 import { createBrowserRouter } from "react-router-dom";
 
 
 /**
- * @createBrouserRouter recibve un array de objetos cada objeto tiene 2 propiedades
+ * @createBrouserRouter recibe un array de objetos cada objeto tiene 2 propiedades..
  * @path la ruta
  * @element el elemento (componenete de pagina q se renderizara en esa ruta)
  */
@@ -17,9 +20,39 @@ export const router = createBrowserRouter([
         path: "/", 
         element: <IndexLayout/>,
         children: [
-            {path: "/", element:<Index/>},
-            {path: "/hero", element:<Hero1/>},
-            {path: "/signup", element:<Hero2Register/>},
-            {path: "/*", element:<NotFound/>}
-        ]}
-])
+        {
+            index: true, 
+            element:<Index/>
+        },
+        {
+            path: "/*",
+            element: <NotFound/>
+        },
+        ]
+    },
+    {
+        path: "/",
+        element:<MainLayout/>,
+        children: [
+            {
+                path: "/signup", 
+                element: <Hero2Register/>
+            },
+            {
+                path:"/signin",
+                element:<Auth/>
+            },
+            {
+                path:"/*",
+                element:<NotFound/>  
+            },             
+        ],
+    },
+    {
+        path: "/*", 
+        element:<NotFound/>
+    },
+]);
+
+
+
