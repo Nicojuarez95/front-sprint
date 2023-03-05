89c14ed31e9062def435533a1f2a7148a60ce22b
Feermeringer <Tu correo>
2023-03-05 14:02:09 -0300

    pull

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -6,79 +6,47 @@ import MainLayouts from '../Layouts/MainLayouts/MainLayouts'
 import CreateManga from '../Components/CreateManga/CreateManga'
 import IndexLayout from "../Layouts/IndexLayouts/IndexLayouts"
 import Auth from "./Auth/Auth";
-import MainLayout from '../Layouts/MainLayouts/MainLayouts'
+
 
 
 import { createBrowserRouter } from "react-router-dom";
 
 
-/**
-<<<<<<< HEAD
- * @createBrouserRouter recibe un array de objetos cada objeto tiene 2 propiedades
-=======
- * @createBrouserRouter recibe un array de objetos cada objeto tiene 2 propiedades..
->>>>>>> b2217f8eae3c7e580cb2168133db6252429604ef
- * @path la ruta
- * @element el elemento (componenete de pagina q se renderizara en esa ruta)
- */
+
 export const router = createBrowserRouter([
+
     {
-        path: "/", 
-        element: <IndexLayout/>,
+        path: "/",
+        element: <IndexLayout />,
         children: [
-<<<<<<< HEAD
-            {path: "/", element:<Index/>},
-            {path: "/hero", element:<Hero1/>},
-            {path: "/signup", element:<Hero2Register/>},
-        ]},
-        {
-        path: '/',
-        element: <MainLayouts/> ,
-        children: [
-
-            {path: "/CreateManga", element:<CreateManga/>},
-        ]
-    },
-        
-        
-        {path: "/*", element:<NotFound/>}
-
-])
-=======
-        {
-            index: true, 
-            element:<Index/>
-        },
-        {
-            path: "/*",
-            element: <NotFound/>
-        },
+            { path: "/", element: <Index /> },
+            { path: "/hero", element: <Hero1 /> },
+            { path: "/signup", element: <Hero2Register /> },
         ]
     },
     {
         path: "/",
-        element:<MainLayout/>,
+        element: <MainLayouts />,
         children: [
             {
-                path: "/signup", 
-                element: <Hero2Register/>
+                path: "/signup",
+                element: <Hero2Register />
             },
             {
-                path:"/signin",
-                element:<Auth/>
+                path: "/signin",
+                element: <Auth />
             },
+            { path: "/CreateManga", element: <CreateManga /> },
+
             {
-                path:"/*",
-                element:<NotFound/>  
-            },             
-        ],
-    },
-    {
-        path: "/*", 
-        element:<NotFound/>
-    },
+                path: "/*",
+                element: <NotFound />
+            },
+        ]
+    }
 ]);
 
 
 
->>>>>>> b2217f8eae3c7e580cb2168133db6252429604ef
+
+
