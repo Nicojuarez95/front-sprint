09374f708487fb709b63e8fd662a149fbb7941c4
Nico <juareznico61@gmail.com>
2023-03-10 19:43:44 -0300

    mangas traidos dinamicamente falta filtrar

--- src/Pages/CardMangas/CardMangas.jsx
+++ src/Pages/CardMangas/CardMangas.jsx
@@ -4,17 +4,17 @@ import './cardmangas.css'
 export default function CardMangas(props) {
   return (
     <div className='cart-shonen'>
-                <div className={props.style2}></div>
+                <div className={props?.style2}></div>
                 <div className='text'>
                   <div>
-                    <h3>Superman Comic</h3>
-                    <span className={props.style}>{props.estilo}</span>
+                    <h3>{props.title}</h3>
+                    <span className={props?.style3}>{props?.category}</span>
                   </div>
                   <a href="#">Read</a>
                 </div>
 
                 <div className='img-cart'>
-                    <img className='imgManga' src="./superman.png" alt="" />
+                    <img className='imgManga' src={props.img} alt="" />
                 </div>
               </div>
   )
--- src/Pages/Mangas/Mangas.jsx
+++ src/Pages/Mangas/Mangas.jsx
@@ -1,44 +1,121 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './mangas.css'
 import CardMangas from '../CardMangas/CardMangas'
+import axios from 'axios'
 
 export default function Mangas() {
+
+  const [mangas_v1, setMangas_v1] = useState([])
+  const url= "http://localhost:8000/createmanga/view"
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setMangas_v1(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function returnClassName(value){
+    switch (value){
+      case "640bae73ea123674b00b1a57": 
+      return "kodomo";
+      break;
+      case "640bae73ea123674b00b1a58": 
+      return "shojo";
+      break;
+      case "640bae73ea123674b00b1a59": 
+      return "seinen";
+      break;
+      case "640bae73ea123674b00b1a56": return "shonen";
+      break;
+      default: return "";
+      break;
+    }
+  }
+  function returnStyle(value){
+    switch (value){
+      case "640bae73ea123674b00b1a57": 
+      return "kodomoBarr";
+      break;
+      case "640bae73ea123674b00b1a58": 
+      return "shojoBarr";
+      break;
+      case "640bae73ea123674b00b1a59": 
+      return "seinenBarr";
+      break;
+      case "640bae73ea123674b00b1a56": return "shonenBarr";
+      break;
+      default: return "";
+      break;
+    }
+  }
+  function returnCategory(value){
+    switch (value){
+      case "640bae73ea123674b00b1a57": 
+      return "Kodomo";
+      break;
+      case "640bae73ea123674b00b1a58": 
+      return "Shojo";
+      break;
+      case "640bae73ea123674b00b1a59": 
+      return "Seinen";
+      break;
+      case "640bae73ea123674b00b1a56": 
+      return "Shonen";
+      break;
+      default: return "";
+      break;
+    }
+  }
+
   return (
     <div className='cont-mangas'>
+
         <div className='fondo-mangas'>   
             <h2>Mangas</h2>
             <span><img src="./Search.png" alt="" /><input className='search' type="search" placeholder='Find your manga here' /></span>
         </div>
+
         <div className='section-manga'>
+
             <div className="cont-checks">
               <label class="category-button">
                 <input type="checkbox" name="category" value="all"/>
-                <span class="category-label">All</span>
+                <span className="category-label">All</span>
               </label>
               <label class="category-button2">
                 <input type="checkbox" name="category" value="all"/>
-                <span class="category-label">Shōnen</span>
+                <span className="category-label">Shōnen</span>
               </label>
               <label class="category-button3">
                 <input type="checkbox" name="category" value="all"/>
-                <span class="category-label">Seinen</span>
+                <span className="category-label">Seinen</span>
               </label>
               <label class="category-button4">
                 <input type="checkbox" name="category" value="all"/>
-                <span class="category-label">Shōjo</span>
+                <span className="category-label">Shōjo</span>
               </label>
               <label class="category-button5">
                 <input type="checkbox" name="category" value="all"/>
-                <span class="category-label">Kodomo</span>
+                <span className="category-label">Kodomo</span>
               </label>
             </div>
 
             <div className='cont-cartas'>
-            <CardMangas style="textspan" style2="barra" estilo="Kodomo"/>
-            <CardMangas style="textspan2" style2="barra2" estilo="Shōjo"/>
-            <CardMangas style="textspan3" style2="barra3" estilo="Seinen"/>
-            <CardMangas style="textspan4" style2="barra4" estilo="Shōnen"/>
-
+              {mangas_v1.mangas?.map((manga, index) =>(
+                <CardMangas 
+                key={index} 
+                style2={returnStyle(manga.category_id)}
+                style3={returnClassName(manga.category_id)}
+                category={returnCategory(manga.category_id)} 
+                title={manga.title}
+                img={manga.cover_photo}/>
+              ))}
             </div>
 
         </div>
