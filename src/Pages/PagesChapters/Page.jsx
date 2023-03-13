0d42490c75e77fc68aef08d44756c4a06acdbb5a
Feermeringer <Tu correo>
2023-03-13 18:12:30 -0300

    vista renderizada

--- src/Pages/PagesChapters/Page.jsx
+++ src/Pages/PagesChapters/Page.jsx
@@ -1,67 +1,75 @@
+import React from 'react'
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
-import  './Page.css';
+import { useNavigate, useParams} from "react-router-dom";
+import './Page.css';
 
 
 export default function Page() {
-  const [chapter, setChapter] = useState(null);
-  const { id, page } = useParams();
   const navigate = useNavigate();
-  const pageNumber = parseFloat(page);
-  
+  const { id, page } = useParams();
+  const url = 'http://localhost:8000/chapters/';
+  const [chapter, setChapter] = useState({});
+  const [next, setNext] = useState('');
+  let [index, setIndex] = useState(Number(page));
+
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8000/chapters/`+ id)
-      .then((response) => setChapter(response.data.Chapter))
+      .get(`${url}${id}`)
+      .then(response => {
+        setChapter(response.data.chapter);
+        setNext(response.data.next);
+        
+
+      }
+      )
       .catch((error) => console.error(error));
-  }, [id]);
+  }, []);
 
-  const goNext = () => {
-    if (pageNumber === chapter.pages.length) {
-      navigate(`/chapters/${id}/${1}`);
-    } else {
-      navigate(`/chapters/${id}/${pageNumber + 1}`);
+  const handlePrev = () => {
+    setIndex(index - 1)
+    navigate(`/chapters/${id}/${index - 1}`)
+    if (index <= 0) {
+        navigate(`/manga/${chapter.manga_id}/`)
     }
-  };
+}
 
-  const goBack = () => {
-    if (pageNumber === 1) {
-      navigate(`/chapters/${id}/${chapter.pages.length}`);
-    } else {
-      navigate(`/chapters/${id}/${pageNumber - 1}`);
-    }
-  };
+const handleNext = () => {
+    setIndex(index + 1)
+    navigate(`/chapters/${id}/${index + 1}`)
+    if (index >= chapter.pages.length -1) {
+        navigate(`/chapters/${next}/${0}`)
 
-  return (
-    <div className="mover">
-      <div className="div-chapter2">
-          <p className="parrafo-chapter2"> Cap N° {chapter.order} - {chapter.title} </p>
-      </div>
-      <div className="contenedor-capitulos">
-        <button className="boton-back" onClick={goBack}>
-          <img className="flecha" src='/flecha-izquierda.png' alt="" />
-        </button>
+}
+}
 
-        {chapter ? (
-          <img className="posi" src={chapter.pages[page - 1]}></img>
-        ) : (
-          "no existe el capitulo"
-        )}
 
-        <button className="boton-next" onClick={goNext}>
-          <img className="flecha" src='/flecha-correcta.png' alt="" />
-        </button>
-      </div>
-      <div className="div-chapter3">
-        <div className="chapter3">
-          <p className="parrafo-chapter3">
-            {" "}
-            <img className="comment" src='/icon_comment.png' alt="" /> numero del capítulo
-          </p>
+return (
+  <div className="mover">
+    <div className="div-chapter2">
+      <p className="parrafo-chapter2"> Cap N°  {chapter.order} - {chapter.title} </p>
+    </div>
+    <div className="contenedor-capitulos">
+      <button className="boton-back" onClick={handlePrev}>
+        <img className="flecha" src='/flecha-izquierda.png' alt="" />
+      </button>
+
+      <div className='posi'>
+          <img className='imgChapter' src={chapter?.pages?.[index]} alt="" />
         </div>
+
+      <button className="boton-next" onClick={handleNext}>
+        <img className="flecha" src='/flecha-correcta.png' alt="" />
+      </button>
+    </div>
+    <div className="div-chapter3">
+      <div className="chapter3">
+        <p className="parrafo-chapter3">
+          <img className="comment" src='/icon_comment.png' alt="" /> numero del capítulo
+        </p>
       </div>
     </div>
-  );
+  </div>
+);
 }
