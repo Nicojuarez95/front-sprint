a24f4504eec5229e39e3a0062766318133340f46
Feermeringer <Tu correo>
2023-03-15 17:22:31 -0300

    modificaciones

--- src/Components/NavIndex/NavIndex.jsx
+++ src/Components/NavIndex/NavIndex.jsx
@@ -90,7 +90,7 @@ export default function NavIndex({handleRender}) {
             <div className='ancors-nav'>
                 <Anchor to="/">Home</Anchor>
                 <Anchor to="/mangas">Mangas</Anchor>
-                <Anchor to="/createmanga">My mangas</Anchor>
+                <Anchor to="/mangas-form">My mangas</Anchor>
                 <Anchor to="#">Favorites</Anchor>
                 { token ? <Anchor to="/author">Author</Anchor> : ""}
                 { token ? <Anchor onClick={handleLogout}>Logout</Anchor> : ""}
